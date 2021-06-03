f182d83bca1b0cf6ea43defb79edd871c99fa4ab
Amnaaaaa <81854874+Amnaaaaa@users.noreply.github.com>
2021-06-03 18:15:21 +0500

    class commits

---
+++ src/mess/management/system/Worker.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mess.management.system;
+
+/**
+ *
+ * @author Hp
+ */
+public class Worker extends Manager {
+    
+}
